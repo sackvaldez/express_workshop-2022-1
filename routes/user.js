543c85fd1184566aabb0f5e65b975b9e7b256c5c
sackvaldez <rauldaxo@gmail.com>
2022-05-14 19:29:06 -0500

    actividad 15 cambios

--- routes/user.js
+++ routes/user.js
@@ -4,22 +4,21 @@ const user= express.Router();
 const db= require('../config/database');
 
 user.post("/signin", async (req,res,next)=>{
-    const {user_name, user_email, user_password}= req.body;
+    const {user_name, user_mail, user_password}= req.body;
     
-    if(user_name && user_email && user_password){
+    if(user_name && user_mail && user_password){
 
         let query= "INSERT INTO user1 (user_name, user_mail, user_password)";
-        query += `VALUES ('${user_name}','${user_email}','${user_password}');`;
-    
+        query += `VALUES ('${user_name}','${user_mail}','${user_password}');`;
         const rows= await db.query(query);
     
         if(rows.affectedRows == 1) {
-            return res.status(201).json({ode: 201, message: "Usuario registrado correctamente"});
+            return res.status(201).json({code: 201, message: "Usuario registrado correctamente"});
         }
-        return res.status(500).json({code: 500, message: "Ocurrió un error"})
+        return res.status(500).json({code: 500, message: "Ocurrió un error"});
 
     }
-    return res.status(500).json({code: 500, message: "OJO: Campos incompletos "})
+    return res.status(500).json({code: 500, message: "OJO: Campos incompletos "});
 
 });
 
@@ -38,7 +37,7 @@ user.post("/login", async (req, res, next) => {
         return res.status(200).json({code: 200, message: token});
     } 
     else {
-        return res.status(200).json({code: 200, message: "Usuario o contraseña incorrectos"});
+        return res.status(200).json({code: 401, message: "Usuario o contraseña incorrectos"});
         }
 
     }
@@ -52,7 +51,5 @@ user.get("/", async (req,res, next)=>{
 
     return res.status(200).json({code: 200, message: rows});
 });
-
-
 
 module.exports = user; 
